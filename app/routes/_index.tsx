2daa13e72cc3e08600298701109fd85460995179
smarthug <kirklayer@gmail.com>
2024-04-02 10:43:36 +0900

    init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
+      <h1>Welcome to Remix test (with Vite and Cloudflare)</h1>
       <ul>
         <li>
           <a
