97fed83635ad937928f215fd23b71abe7dd42412
smarthug <kirklayer@gmail.com>
2024-04-03 14:35:08 +0900

    github done

---
+++ app/routes/auth.github.tsx
@@ -0,0 +1,12 @@
+// app/routes/auth/github.tsx
+import type { ActionFunctionArgs } from "@remix-run/cloudflare";
+import { redirect } from "@remix-run/cloudflare";
+import { authenticator } from "~/auth.server";
+
+export async function loader() {
+  return redirect("/login");
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  return authenticator.authenticate("github", request);
+};
