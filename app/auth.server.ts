4ffc071445bbda23f52f9ad418a14337e737c2bc
smarthug <kirklayer@gmail.com>
2024-04-05 11:53:15 +0900

    🎉: cloudflare pages deployed github login works

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -1,21 +1,46 @@
-import { createCookieSessionStorage } from "@remix-run/cloudflare";
+import {
+  createCookieSessionStorage,
+  LoaderFunctionArgs,
+} from "@remix-run/cloudflare";
 import { Authenticator } from "remix-auth";
 import type { GitHubExtraParams, GitHubProfile } from "remix-auth-github";
 import { GitHubStrategy } from "remix-auth-github";
 
-if (!process.env.GITHUB_CLIENT_ID) {
-  throw new Error("GITHUB_CLIENT_ID is required");
-}
+// if (!process.env.GITHUB_CLIENT_ID) {
+//   throw new Error("GITHUB_CLIENT_ID is required");
+// }
 
-if (!process.env.GITHUB_CLIENT_SECRET) {
-  throw new Error("GITHUB_CLIENT_SECRET is required");
-}
+// if (!process.env.GITHUB_CLIENT_SECRET) {
+//   throw new Error("GITHUB_CLIENT_SECRET is required");
+// }
 
-if (!process.env.BASE_URL) {
-  throw new Error("BASE_URL is required");
-}
+// if (!process.env.BASE_URL) {
+//   throw new Error("BASE_URL is required");
+// }
 
-const BASE_URL = process.env.BASE_URL;
+// const BASE_URL = process.env.BASE_URL;
+
+let GITHUB_CLIENT_ID = "";
+let GITHUB_CLIENT_SECRET = "";
+let BASE_URL = "";
+
+// export const loader =
+//   async () =>
+//   ({ context }: LoaderFunctionArgs) => {
+//     console.log("this is context.env.GITHUB_CLIENT_ID");
+//     console.log(context.env.GITHUB_CLIENT_ID);
+//     // GITHUB_CLIENT_ID = context.env.GITHUB_CLIENT_ID;
+//     // GITHUB_CLIENT_SECRET = context.env.GITHUB_CLIENT_SECRET;
+//     // BASE_URL = context.env.BASE_URL;
+//   };
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  console.log("this is context.env.GITHUB_CLIENT_ID");
+  console.log(context.cloudflare.env.GITHUB_CLIENT_ID);
+  // GITHUB_CLIENT_ID = context.env.GITHUB_CLIENT_ID;
+  // GITHUB_CLIENT_SECRET = context.env.GITHUB_CLIENT_SECRET;
+  // BASE_URL = context.env.BASE_URL;
+}
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -24,7 +49,8 @@ export const sessionStorage = createCookieSessionStorage({
     path: "/",
     sameSite: "lax",
     secrets: ["s3cret"], // This should be an env variable
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
+    secure: true,
   },
 });
 
@@ -34,17 +60,17 @@ export const authenticator = new Authenticator<{
   extraParams: GitHubExtraParams;
 }>(sessionStorage);
 
-authenticator.use(
-  new GitHubStrategy(
-    {
-      clientID: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      callbackURL: new URL("/auth/github/callback", BASE_URL).toString(),
-    },
-    async ({ profile, accessToken, extraParams }) => {
-      console.log("this is accesstoken");
-      console.log(accessToken);
-      return { profile, accessToken, extraParams };
-    }
-  )
-);
+// authenticator.use(
+//   new GitHubStrategy(
+//     {
+//       clientID: GITHUB_CLIENT_ID,
+//       clientSecret: GITHUB_CLIENT_SECRET,
+//       callbackURL: new URL("/auth/github/callback", BASE_URL).toString(),
+//     },
+//     async ({ profile, accessToken, extraParams }) => {
+//       console.log("this is accesstoken");
+//       console.log(accessToken);
+//       return { profile, accessToken, extraParams };
+//     }
+//   )
+// );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,41 @@
-
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { Form } from "@remix-run/react";
+
+import { authenticator } from "~/auth.server";
+import { GitHubStrategy } from "remix-auth-github";
 
 // import { auth, sessionStorage } from "~/auth.server";
 
+export async function loader({ context }: LoaderFunctionArgs) {
+  console.log("this is context.env.GITHUB_CLIENT_ID");
+  // console.log(context.env.GITHUB_CLIENT_ID);
+  // console.log(context); // GITHUB_CLIENT_ID = context.env.GITHUB_CLIENT_ID;
+  // GITHUB_CLIENT_SECRET = context.env.GITHUB_CLIENT_SECRET;
+  // BASE_URL = context.env.BASE_URL;
+  console.log(context.cloudflare.env);
+
+  authenticator.use(
+    new GitHubStrategy(
+      {
+        clientID: context.cloudflare.env.GITHUB_CLIENT_ID,
+        clientSecret: context.cloudflare.env.GITHUB_CLIENT_SECRET,
+        callbackURL: new URL("/auth/github/callback", context.cloudflare.env.BASE_URL).toString(),
+      },
+      async ({ profile, accessToken, extraParams }) => {
+        console.log("this is accesstoken");
+        console.log(accessToken);
+        return { profile, accessToken, extraParams };
+      }
+    )
+  );
+
+  return null;
+}
 
 export default function Index() {
- 
-
   return (
     <Form action="/auth/github" method="post">
-    <button>Login with GitHub</button>
-  </Form>
+      <button>Login with GitHub</button>
+    </Form>
   );
 }
