276a9020f87170c373d8644d6f7f59dcfc012067
smarthug <kirklayer@gmail.com>
2024-04-09 13:20:45 +0900

    get code

--- app/routes/auth.facebook.callback.tsx
+++ app/routes/auth.facebook.callback.tsx
@@ -1,3 +1,33 @@
+// React 컴포넌트에서 리디렉션 처리 예
+// import React, { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+
+// const FacebookCallbackPage = () => {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const queryParams = new URLSearchParams(location.search);
+//     const code = queryParams.get('code');
+//     const state = queryParams.get('state');
+
+//     if (code) {
+//       // 백엔드로 code 전송하여 액세스 토큰 요청
+//     }
+//   }, [location]);
+
+//   return (
+//     <div>
+//       로그인 처리 중...
+//     </div>
+//   );
+// };
+
+// export default FacebookCallbackPage;
+
+
+
+
+
 // app/routes/auth/github/callback.tsx
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { authenticator } from "~/auth.server";
--- app/routes/auth.facebook.tsx
+++ app/routes/auth.facebook.tsx
@@ -1,15 +1,38 @@
 // app/routes/auth/github.tsx
-import type { ActionFunctionArgs } from "@remix-run/cloudflare";
+import { ActionFunctionArgs, redirect } from "@remix-run/cloudflare";
 // import { redirect } from "@remix-run/cloudflare";
 import { authenticator } from "~/auth.server";
+
+import { v4 as uuid } from "uuid";
 
 // export async function loader() {
 //   return redirect("/");
 // }
 
+// export async function action({ request }: ActionFunctionArgs) {
+//   return authenticator.authenticate("facebook", request,{
+//     successRedirect: "/private",
+//     failureRedirect: "/",
+//   });
+// };
+
+
+
+// const facebookLogin = () => {
+//   const client_id = "YOUR_APP_ID";
+//   const redirect_uri = "http://localhost:3000/facebook-callback";
+//   const state = "{st=state123abc,ds=123456789}"; // 상태 토큰은 CSRF 공격을 방지하기 위해 사용됩니다.
+//   const scope = "email,public_profile"; // 요청할 권한
+
+//   window.location.href = `https://www.facebook.com/v14.0/dialog/oauth?client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}`;
+// };
+
 export async function action({ request }: ActionFunctionArgs) {
-  return authenticator.authenticate("facebook", request,{
-    successRedirect: "/private",
-    failureRedirect: "/",
-  });
+  const client_id = "1633084590837571";
+  const redirect_uri = "https://my-remix-app-cf-github.pages.dev/auth/facebook/callback";
+  const state = uuid(); // 상태 토큰은 CSRF 공격을 방지하기 위해 사용됩니다.
+  const scope = "email,public_profile"; // 요청할 권한
+
+  console.log(`https://www.facebook.com/v14.0/dialog/oauth?client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}&response_type=code`)
+  throw redirect(`https://www.facebook.com/v14.0/dialog/oauth?client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}&response_type=code`)
 };
