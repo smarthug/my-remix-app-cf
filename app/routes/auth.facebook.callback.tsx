13394c60f0ccf4930a284326405cbc1fffb47a35
smarthug <kirklayer@gmail.com>
2024-04-09 13:26:49 +0900

    fb test

--- app/routes/auth.facebook.callback.tsx
+++ app/routes/auth.facebook.callback.tsx
@@ -1,40 +1,43 @@
 // React 컴포넌트에서 리디렉션 처리 예
-// import React, { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-
-// const FacebookCallbackPage = () => {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const queryParams = new URLSearchParams(location.search);
-//     const code = queryParams.get('code');
-//     const state = queryParams.get('state');
-
-//     if (code) {
-//       // 백엔드로 code 전송하여 액세스 토큰 요청
-//     }
-//   }, [location]);
-
-//   return (
-//     <div>
-//       로그인 처리 중...
-//     </div>
-//   );
-// };
-
-// export default FacebookCallbackPage;
+import React, { useEffect } from 'react';
+import { useLocation } from "@remix-run/react";
 
 
+const FacebookCallbackPage = () => {
+  const location = useLocation();
 
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const code = queryParams.get('code');
+    const state = queryParams.get('state');
 
+    if (code) {
+      // 백엔드로 code 전송하여 액세스 토큰 요청
+      console.log('code:', code);
+      console.log('state:', state);
+    }
+  }, [location]);
 
-// app/routes/auth/github/callback.tsx
-import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { authenticator } from "~/auth.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  return authenticator.authenticate("facebook", request, {
-    successRedirect: "/private",
-    failureRedirect: "/",
-  });
+  return (
+    <div>
+      로그인 처리 중...
+    </div>
+  );
 };
+
+export default FacebookCallbackPage;
+
+
+
+
+
+// // app/routes/auth/github/callback.tsx
+// import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
+// import { authenticator } from "~/auth.server";
+
+// export async function loader({ request }: LoaderFunctionArgs) {
+//   return authenticator.authenticate("facebook", request, {
+//     successRedirect: "/private",
+//     failureRedirect: "/",
+//   });
+// };
